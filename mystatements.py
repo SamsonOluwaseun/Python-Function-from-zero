ed875bf8e0d52c9110a01d6d96810fe2b5a8034f
SamsonOluwaseun <39919583+SamsonOluwaseun@users.noreply.github.com>
2024-01-09 22:53:55 +0000

    my update

--- mystatements.py
+++ mystatements.py
@@ -1,11 +1,11 @@
-mylist= ["fruit","apple","cherry"]
-my_dict={"drink":"coffee","milk":"whole"}
+mylist = ["fruit", "apple", "cherry"]
+my_dict = {"drink": "coffee", "milk": "whole"}
 
 for item in mylist:
     print(f"my favourite thing to eat is: {item}")
 
-for _,value in my_dict.items():
+for _, value in my_dict.items():
     print(f"My favourite thing to dring is: {value}")
 
-#my_dict=my_dict
-#my_list3=
+# my_dict=my_dict
+# my_list3=
